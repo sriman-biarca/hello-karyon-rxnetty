7b378b836625bb18d98c57c13d64f7d964753cf7
sriman-biarca <srimanc@biarca.com>
2018-04-26 13:01:23 +0530

    Update doc-build.sh

--- doc-build.sh
+++ doc-build.sh
@@ -16,10 +16,21 @@ gcloud --quiet version
 
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > $HOME/spin-terra.json
 
-echo $GCLOUD_SERVICE_KEY_PRD
 
-cat $HOME/spin-terra.json
 gcloud auth activate-service-account --key-file=$HOME/spin-terra.json
 
 gcloud --quiet config set project spinnaker-terraform
-gcloud docker push gcr.io/spinnaker-terraform/hello-code
+
+VERSION=1.4.3
+OS=linux  # or "darwin" for OSX, "windows" for Windows.
+ARCH=amd64  # or "386" for 32-bit OSs
+
+curl -L "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v${VERSION}/docker-credential-gcr_${OS}_${ARCH}-${VERSION}.tar.gz" \
+  | tar xz --to-stdout docker-credential-gcr \
+  > /usr/bin/docker-credential-gcr && chmod +x /usr/bin/docker-credential-gcr
+
+export CLOUDSDK_CORE_DISABLE_PROMPTS=1
+gcloud components install docker-credential-gcr
+docker-credential-gcr configure-docker
+
+gcloud docker -- push gcr.io/spinnaker-terraform/hello-code
